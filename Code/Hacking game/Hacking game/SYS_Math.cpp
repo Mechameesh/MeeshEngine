cfdbcdbae6bb41c24d0b5783eafa2f39395ded43
Meesh <Meesh@home>
2012-08-20 13:27:44 +0200

    Added vector and matrix operations

--- Code/Hacking game/Hacking game/SYS_Math.cpp
+++ Code/Hacking game/Hacking game/SYS_Math.cpp
@@ -5,7 +5,7 @@
 
 /******************************************************************/
 
-void MATH_Vector2Add(math_vec2 * res, math_vec2 * a, math_vec2 * b)
+void MATH_Vector2Add(math_vec2 * res, const math_vec2 * a, const math_vec2 * b)
 {
 	float x = a->x + b->x;
 	float y = a->y + b->y;
@@ -13,7 +13,7 @@ void MATH_Vector2Add(math_vec2 * res, math_vec2 * a, math_vec2 * b)
 	res->y = y;
 }
 
-void MATH_Vector2Subtract(math_vec2 * res, math_vec2 * a, math_vec2 * b)
+void MATH_Vector2Subtract(math_vec2 * res, const math_vec2 * a, const math_vec2 * b)
 {
 	float x = a->x - b->x;
 	float y = a->y - b->y;
@@ -21,7 +21,7 @@ void MATH_Vector2Subtract(math_vec2 * res, math_vec2 * a, math_vec2 * b)
 	res->y = y;
 }
 
-void MATH_Vector2Scale(math_vec2 * res, math_vec2 * in, float s)
+void MATH_Vector2Scale(math_vec2 * res, const math_vec2 * in, float s)
 {
 	float x = in->x * s;
 	float y = in->y * s;
@@ -29,12 +29,12 @@ void MATH_Vector2Scale(math_vec2 * res, math_vec2 * in, float s)
 	res->y = y;
 }
 
-float MATH_Vector2Dot(math_vec2 * a, math_vec2 * b)
+float MATH_Vector2Dot(const math_vec2 * a, const math_vec2 * b)
 {
 	return (a->x * b->x) + (a->y * b->y);
 }
 
-void MATH_Vector2Invert(math_vec2 * res, math_vec2 * in)
+void MATH_Vector2Invert(math_vec2 * res, const math_vec2 * in)
 {
 	float x = -in->x;
 	float y = -in->y;
@@ -42,14 +42,22 @@ void MATH_Vector2Invert(math_vec2 * res, math_vec2 * in)
 	res->y = y;
 }
 
-float MATH_Vector2Length(math_vec2 * in)
+float MATH_Vector2Length(const math_vec2 * in)
 {
 	return sqrtf((in->x * in->x) + (in->y * in->y));
 }
 
+float MATH_Vector2Normalise(math_vec2 * res, const math_vec2 * in)
+{
+	float len = sqrtf((in->x * in->x) + (in->y * in->y));
+	res->x = in->x / len;
+	res->y = in->y / len;
+	return len;
+}
+
 /******************************************************************/
 
-void MATH_Vector3Add(math_vec3 * res, math_vec3 * a, math_vec3 * b)
+void MATH_Vector3Add(math_vec3 * res, const math_vec3 * a, const math_vec3 * b)
 {
 	float x = a->x + b->x;
 	float y = a->y + b->y;
@@ -59,7 +67,7 @@ void MATH_Vector3Add(math_vec3 * res, math_vec3 * a, math_vec3 * b)
 	res->z = z;
 }
 
-void MATH_Vector3Subtract(math_vec3 * res, math_vec3 * a, math_vec3 * b)
+void MATH_Vector3Subtract(math_vec3 * res, const math_vec3 * a, const math_vec3 * b)
 {
 	float x = a->x - b->x;
 	float y = a->y - b->y;
@@ -68,7 +76,7 @@ void MATH_Vector3Subtract(math_vec3 * res, math_vec3 * a, math_vec3 * b)
 	res->y = y;
 	res->z = z;
 }
-void MATH_Vector3Scale(math_vec3 * res, math_vec3 * in, float s)
+void MATH_Vector3Scale(math_vec3 * res, const math_vec3 * in, float s)
 {
 	float x = in->x * s;
 	float y = in->y * s;
@@ -78,7 +86,7 @@ void MATH_Vector3Scale(math_vec3 * res, math_vec3 * in, float s)
 	res->z = z;
 }
 
-float MATH_Vector3Dot(math_vec3 * a, math_vec3 * b)
+float MATH_Vector3Dot(const math_vec3 * a, const math_vec3 * b)
 {
 	return (a->x * b->x) + (a->y * b->y) + (a->z * b->z);
 }
@@ -93,9 +101,18 @@ void MATH_Vector3Cross(math_vec3 * res, math_vec3 * a, math_vec3 * b)
 	res->z = z;
 }
 
-float MATH_Vector3Length(math_vec3 * in)
+float MATH_Vector3Length(const math_vec3 * in)
 {
 	return sqrtf((in->x * in->x) + (in->y * in->y) + (in->z * in->z));
+}
+
+float MATH_Vector3Normalise(math_vec3 * res, const math_vec3 * in)
+{
+	float len = sqrtf((in->x * in->x) + (in->y * in->y) + (in->z * in->z));
+	res->x = in->x / len;
+	res->y = in->y / len;
+	res->z = in->z / len;
+	return len;
 }
 
 /******************************************************************/
@@ -107,21 +124,20 @@ void MATH_Matrix3Copy(math_matrix3 * res, const math_matrix3 * in)
 			res->m[i][j] = in->m[i][j];
 }
 
-void MATH_Matrix3Identity(math_matrix3 * in)
+void MATH_Matrix3Identity(math_matrix3 * res)
 {
-	in->m[0][0] = 1.0f;
-	in->m[0][1] = 0.0f;
-	in->m[0][2] = 0.0f;
-	in->m[1][0] = 0.0f;
-	in->m[1][1] = 1.0f;
-	in->m[1][2] = 0.0f;
-	in->m[2][0] = 0.0f;
-	in->m[2][1] = 0.0f;
-	in->m[2][2] = 1.0f;
-	
+	res->m[0][0] = 1.0f;
+	res->m[0][1] = 0.0f;
+	res->m[0][2] = 0.0f;
+	res->m[1][0] = 0.0f;
+	res->m[1][1] = 1.0f;
+	res->m[1][2] = 0.0f;
+	res->m[2][0] = 0.0f;
+	res->m[2][1] = 0.0f;
+	res->m[2][2] = 1.0f;	
 }
 
-void MATH_Matrix3Concat(math_matrix3 * res, math_matrix3 * a, math_matrix3 * b)
+void MATH_Matrix3Concat(math_matrix3 * res, const math_matrix3 * a, const math_matrix3 * b)
 {
 	float m00 = (a->m[0][0] * b->m[0][0]) + (a->m[0][1] * b->m[1][0]) + (a->m[0][2] * b->m[2][0]);
 	float m01 = (a->m[0][0] * b->m[0][1]) + (a->m[0][1] * b->m[1][1]) + (a->m[0][2] * b->m[2][1]);
@@ -144,7 +160,7 @@ void MATH_Matrix3Concat(math_matrix3 * res, math_matrix3 * a, math_matrix3 * b)
 	res->m[2][2] = m22;
 }
 
-void MATH_Matrix3Transpose(math_matrix3 * res, math_matrix3 * in)
+void MATH_Matrix3Transpose(math_matrix3 * res, const math_matrix3 * in)
 {	
 	float m01 = in->m[1][0];
 	float m02 = in->m[2][0];	
@@ -173,27 +189,27 @@ void MATH_Matrix4Copy(math_matrix4 * res, const math_matrix4 * in)
 			res->m[i][j] = in->m[i][j];
 }
 
-void MATH_Matrix4Identity(math_matrix4 * in)
+void MATH_Matrix4Identity(math_matrix4 * res)
 {
-	in->m[0][0] = 1.0f;
-	in->m[0][1] = 0.0f;
-	in->m[0][2] = 0.0f;
-	in->m[0][3] = 0.0f;
-	in->m[1][0] = 0.0f;
-	in->m[1][1] = 1.0f;
-	in->m[1][2] = 0.0f;
-	in->m[1][3] = 0.0f;
-	in->m[2][0] = 0.0f;
-	in->m[2][1] = 0.0f;
-	in->m[2][2] = 1.0f;
-	in->m[2][3] = 0.0f;
-	in->m[3][0] = 0.0f;
-	in->m[3][1] = 0.0f;
-	in->m[3][2] = 0.0f;
-	in->m[3][3] = 1.0f;
+	res->m[0][0] = 1.0f;
+	res->m[0][1] = 0.0f;
+	res->m[0][2] = 0.0f;
+	res->m[0][3] = 0.0f;
+	res->m[1][0] = 0.0f;
+	res->m[1][1] = 1.0f;
+	res->m[1][2] = 0.0f;
+	res->m[1][3] = 0.0f;
+	res->m[2][0] = 0.0f;
+	res->m[2][1] = 0.0f;
+	res->m[2][2] = 1.0f;
+	res->m[2][3] = 0.0f;
+	res->m[3][0] = 0.0f;
+	res->m[3][1] = 0.0f;
+	res->m[3][2] = 0.0f;
+	res->m[3][3] = 1.0f;
 }
 
-void MATH_Matrix4Concat(math_matrix4 * res, math_matrix4 * a, math_matrix4 * b)
+void MATH_Matrix4Concat(math_matrix4 * res, const math_matrix4 * a, const math_matrix4 * b)
 {
 	float m00 = (a->m[0][0] * b->m[0][0]) + (a->m[0][1] * b->m[1][0]) + (a->m[0][2] * b->m[2][0]) + (a->m[0][3] * b->m[3][0]);
 	float m01 = (a->m[0][0] * b->m[0][1]) + (a->m[0][1] * b->m[1][1]) + (a->m[0][2] * b->m[2][1]) + (a->m[0][3] * b->m[3][1]);
@@ -252,7 +268,7 @@ void MATH_Matrix4Concat(math_matrix4 * res, math_matrix4 * a, math_matrix4 * b)
 	in->m[3][3] = 1.0f;	
 }*/
 
-void MATH_Matrix4Transpose(math_matrix4 * res, math_matrix4 * in)
+void MATH_Matrix4Transpose(math_matrix4 * res, const math_matrix4 * in)
 {	
 	float m01 = in->m[1][0];
 	float m02 = in->m[2][0];
@@ -336,3 +352,104 @@ void MATH_Matrix4Ortho(math_matrix4 * res, int width, int height, float znear, f
 }
 
 /******************************************************************/
+
+void MATH_Matrix4Rotate(math_matrix4 * res, const int axis, const float amount)
+{
+	float c = cosf(amount);
+	float s = sinf(amount);
+
+	switch(axis)
+	{
+	case MATH_AXIS_X:
+		{
+			res->m[0][0] = 1.0f;
+			res->m[0][1] = 0.0f;
+			res->m[0][2] = 0.0f;
+			res->m[0][3] = 0.0f;
+			res->m[1][0] = 0.0f;
+			res->m[1][1] = c;
+			res->m[1][2] = -s;
+			res->m[1][3] = 0.0f;
+			res->m[2][0] = 0.0f;
+			res->m[2][1] = s;
+			res->m[2][2] = c;
+			res->m[2][3] = 0.0f;
+			res->m[3][0] = 0.0f;
+			res->m[3][1] = 0.0f;
+			res->m[3][2] = 0.0f;
+			res->m[3][3] = 1.0f;
+
+		} break;
+	case MATH_AXIS_Y:
+		{
+			res->m[0][0] = c;
+			res->m[0][1] = 0.0f;
+			res->m[0][2] = s;
+			res->m[0][3] = 0.0f;
+			res->m[1][0] = 0.0f;
+			res->m[1][1] = 1.0f;
+			res->m[1][2] = 0.0f;
+			res->m[1][3] = 0.0f;
+			res->m[2][0] = -s;
+			res->m[2][1] = 0.0f;
+			res->m[2][2] = c;
+			res->m[2][3] = 0.0f;
+			res->m[3][0] = 0.0f;
+			res->m[3][1] = 0.0f;
+			res->m[3][2] = 0.0f;
+			res->m[3][3] = 1.0f;
+		} break;
+	case MATH_AXIS_Z:
+		{
+			res->m[0][0] = c;
+			res->m[0][1] = -s;
+			res->m[0][2] = 0.0f;
+			res->m[0][3] = 0.0f;
+			res->m[1][0] = s;
+			res->m[1][1] = c;
+			res->m[1][2] = 0.0f;
+			res->m[1][3] = 0.0f;
+			res->m[2][0] = 0.0f;
+			res->m[2][1] = 0.0f;
+			res->m[2][2] = 1.0f;
+			res->m[2][3] = 0.0f;
+			res->m[3][0] = 0.0f;
+			res->m[3][1] = 0.0f;
+			res->m[3][2] = 0.0f;
+			res->m[3][3] = 1.0f;
+		} break;
+	}
+}
+
+void MATH_Matrix4RotateAxis(math_matrix4 * res, const math_vec3 * axis, const float amount)
+{	
+	math_vec3 naxis;
+	MATH_Vector3Normalise(&naxis, axis);
+	
+	float s = sinf(amount);
+	float c = cosf(amount);
+	float cmin = (1.0f - c);
+	float xy = naxis.x * naxis.y;
+	float xz = naxis.x * naxis.z;
+	float yz = naxis.y * naxis.z;
+	float xx = naxis.x * naxis.x;
+	float yy = naxis.y * naxis.y;
+	float zz = naxis.z * naxis.z;
+
+	res->m[0][0] = xx + ((1.0f - xx) * c);
+	res->m[0][1] = (xy * cmin) - (naxis.z * s);
+	res->m[0][2] = (yz * cmin) + (naxis.y * s);
+	res->m[0][3] = 0.0f;
+	res->m[1][0] = (xy * cmin) + (naxis.z * s);
+	res->m[1][1] = yy + ((1.0f - yy)*c);
+	res->m[1][2] = (yz * cmin) - (naxis.x * c);
+	res->m[1][3] = 0.0f;
+	res->m[2][0] = (xz * cmin) - (naxis.y * s);
+	res->m[2][1] = (yz * cmin) + (naxis.x * s);
+	res->m[2][2] = zz + ((1.0f - zz) * c);
+	res->m[2][3] = 0.0f;
+	res->m[3][0] = 0.0f;
+	res->m[3][1] = 0.0f;
+	res->m[3][2] = 0.0f;
+	res->m[3][3] = 1.0f;
+}
--- Code/Hacking game/Hacking game/SYS_Math.h
+++ Code/Hacking game/Hacking game/SYS_Math.h
@@ -6,6 +6,13 @@
 #define M_PI (3.141592653589793f)
 #define M_HALF_PI (1.570796326794897f)
 
+enum
+{
+	MATH_AXIS_X,
+	MATH_AXIS_Y,
+	MATH_AXIS_Z,
+};
+
 struct math_vec2
 {
 	float x, y;
@@ -31,31 +38,37 @@ struct math_matrix4
 	float m[4][4];
 };
 
-void MATH_Vector2Add(math_vec2 * res, math_vec2 * a, math_vec2 * b);
-void MATH_Vector2Subtract(math_vec2 * res, math_vec2 * a, math_vec2 * b);
-void MATH_Vector2Scale(math_vec2 * res, math_vec2 * in, float s);
-float MATH_Vector2Dot(math_vec2 * res, math_vec2 * a, math_vec2 * b);
-void MATH_Vector2Invert(math_vec2 * res, math_vec2 * in);
-float MATH_Vector2Length(math_vec2 * in);
+void MATH_Vector2Add(math_vec2 * res, const math_vec2 * a, const math_vec2 * b);
+void MATH_Vector2Subtract(math_vec2 * res, const math_vec2 * a, const math_vec2 * b);
+void MATH_Vector2Scale(math_vec2 * res, const math_vec2 * in, float s);
+float MATH_Vector2Dot(math_vec2 * res, const math_vec2 * a, const math_vec2 * b);
+void MATH_Vector2Invert(math_vec2 * res, const math_vec2 * in);
+float MATH_Vector2Length(const math_vec2 * in);
+float MATH_Vector2Normalise(math_vec2 * res, const math_vec2 * in);
 
-void MATH_Vector3Add(math_vec3 * res, math_vec3 * a, math_vec3 * b);
-void MATH_Vector3Subtract(math_vec3 * res, math_vec3 * a, math_vec3 * b);
-void MATH_Vector3Scale(math_vec3 * res, math_vec3 * in, float s);
-float MATH_Vector3Dot(math_vec3 * a, math_vec3 * b);
-void MATH_Vector3Cross(math_vec3 * res, math_vec3 * a, math_vec3 * b);
+void MATH_Vector3Add(math_vec3 * res, const math_vec3 * a, const math_vec3 * b);
+void MATH_Vector3Subtract(math_vec3 * res, const math_vec3 * a, const math_vec3 * b);
+void MATH_Vector3Scale(math_vec3 * res, const math_vec3 * in, float s);
+float MATH_Vector3Dot(const math_vec3 * a, const math_vec3 * b);
+void MATH_Vector3Cross(math_vec3 * res, const math_vec3 * a, const math_vec3 * b);
+float MATH_Vector3Length(const math_vec3 * in);
+float MATH_Vector3Normalise(math_vec3 * res, const math_vec3 * in);
 
 void MATH_Matrix3Copy(math_matrix3 * res, const math_matrix3 * in);
-void MATH_Matrix3Identity(math_matrix3 * in);
-void MATH_Matrix3Concat(math_matrix3 * res, math_matrix3 * a, math_matrix3 * b);
-void MATH_Matrix3Transpose(math_matrix3 * res, math_matrix3 * in);
+void MATH_Matrix3Identity(math_matrix3 * res);
+void MATH_Matrix3Concat(math_matrix3 * res, const math_matrix3 * a, const math_matrix3 * b);
+void MATH_Matrix3Transpose(math_matrix3 * res, const math_matrix3 * in);
 
 void MATH_Matrix4Copy(math_matrix4 * res, const math_matrix4 * in);
-void MATH_Matrix4Identity(math_matrix4 * in);
-void MATH_Matrix4Concat(math_matrix4 * res, math_matrix4 * a, math_matrix4 * b);
-void MATH_Matrix4Transpose(math_matrix4 * res, math_matrix4 * in);
+void MATH_Matrix4Identity(math_matrix4 * res);
+void MATH_Matrix4Concat(math_matrix4 * res, const math_matrix4 * a, const math_matrix4 * b);
+void MATH_Matrix4Transpose(math_matrix4 * res, const math_matrix4 * in);
 
 void MATH_Matrix4Projection(math_matrix4 * res, int width, int height, float fov, float near, float far);
 void MATH_Matrix4Ortho(math_matrix4 * res, int width, int height, float near, float far);
+
+void MATH_Matrix4Rotate(math_matrix4 * res, const int axis, const float amount);
+void MATH_Matrix4RotateAxis(math_matrix4 * res, const math_vec3 * axis, const float amount);
 
 /******************************************************/
 
