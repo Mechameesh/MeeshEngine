c2da5a78604ce5b5d99769adb84e11855b260725
Meesh <Meesh@home>
2012-08-20 13:28:06 +0200

    added scene struct

---
+++ Code/Hacking game/Hacking game/Scene.h
@@ -0,0 +1,15 @@
+#ifndef SCENE_H_INCLUDED
+#define SCENE_H_INCLUDED
+
+#include "SCENE_Mesh.h"
+#include "SYS_Math.h"
+
+struct scene_scene
+{	
+	int nmeshes;
+	scene_mesh * meshes;
+};
+
+
+
+#endif //SCENE_H_INCLUDED
